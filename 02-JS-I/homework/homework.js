2b6d8cdd426a819930f29be0b79596e7ae0e83dd
Leitogalarza <112443047+Leitogalarza@users.noreply.github.com>
2022-08-29 20:25:37 -0300

    Update homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = leo prueba cadena;
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 223564;
 
 // Crea una variable booleana:
 const nuevoBool = null;
